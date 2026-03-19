f5fdec2950cfa8ce1e392d8b73cda53d3c130da7
James Issac <jamesissac@Jamess-MacBook-Pro.local>
2026-03-19 00:18:09 -0700

    safe

--- kyc/src/App.jsx
+++ kyc/src/App.jsx
@@ -1,119 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react';
+import FaceID from './pages/profile/FaceID';
+import VerifyID from './pages/profile/VerifyID';
+import FaceIdDone from './pages/profile/FaceIdDone';
+import Verification from './pages/profile/Verification';
+import CameraScan from './pages/profile/CameraScan';
+import ChooseIdentity from './pages/profile/ChooseIdentity';
+import CameraSuccess from './pages/profile/CameraSuccess';
+import VerificationDone from './pages/profile/VerificationDone';
+import VerificationComfirm from './pages/profile/VerificationComfirm';
+import VerificationAdvance from './pages/profile/VerificationAdvance';
+import ChooseVerification from './pages/profile/ChooseVerification';
+import IdentityVerification from './pages/profile/IdentityVerification';
+import GoogleTranslate from './pages/utils/GoogleTranslate';
+
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { UserContextProvider } from '../context/UserContext';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+axios.defaults.baseURL = 'https://tradevisterserver.vercel.app';
+axios.defaults.withCredentials = true;
+//http://localhost:8080 
+import '../src/fonts/fonts.css';
+import '../src/fonts/font-icons.css';
+import '../src/css/bootstrap.min.css';
+import '../src/css/styles.css';
+import '../src/css/swiper-bundle.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { initializeSwiper } from "../src/js/carousel"
+
+import {
+  Preloader,
+} from "../src/pages/utils/Properties"
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    initializeSwiper();
+    Preloader();
+  }, [])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <UserContextProvider>
+        <Router>
+          <Routes>
+            <Route index element={<Verification />} />
+            <Route path='/FaceID' element={<FaceID />} />
+            <Route path='/FaceIdDone' element={<FaceIdDone />} />
+            <Route path='/VerifyID' element={<VerifyID />} />
+            <Route path='/CameraScan' element={<CameraScan />} />
+            <Route path='/Verification' element={<Verification />} />
+            <Route path='/CameraSuccess' element={<CameraSuccess />} />
+            <Route path='/ChooseIdentity' element={<ChooseIdentity />} />
+            <Route path='/VerificationDone' element={<VerificationDone />} />
+            <Route path='/VerificationComfirm' element={<VerificationComfirm />} />
+            <Route path='/VerificationAdvance' element={<VerificationAdvance />} />
+            <Route path='/ChooseVerification' element={<ChooseVerification />} />
+            <Route path='/IdentityVerification' element={<IdentityVerification />} />
+          </Routes>
+        </Router>
+        <Toaster position='top-right' toastOptions={{ duration: 4000 }} />
+        <GoogleTranslate />
+      </UserContextProvider>
     </>
   )
 }
--- kyc/src/main.jsx
+++ kyc/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="170268353832-0fn4qbgklemeb9s0o5elvi99ronia9ov.apps.googleusercontent.com">
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode> 
+  </GoogleOAuthProvider> 
 )
