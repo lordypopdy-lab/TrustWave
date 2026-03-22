d734db939261bc6521a465f366f38f8b3ac22da6
James Isaac <deelordpopdy2@gmail.com>
2026-03-22 16:44:39 +0100

    Update Admin.jsx
    Safe

--- client/src/admin/pages/Admin.jsx
+++ client/src/admin/pages/Admin.jsx
@@ -1663,7 +1663,13 @@ const Admin = () => {
                     <hr style={{ borderColor: "rgba(255,255,255,0.2)" }} />
 
                     {/* Table */}
-                    <div className="table-responsive">
+                    <div 
+                      style={{
+                          maxHeight: "400px",
+                          overflowY: "auto",
+                          borderRadius: "12px",
+                        }}
+                      className="table-responsive">
                       <table className="table table-dark table-hover align-middle">
                         <thead>
                           <tr
